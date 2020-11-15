466c3dc24ebc5bbf59c1f06a6e8c035e64c6f4b7
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-11-15 22:57:58 +0200

    Create 7-8 object.js

---
+++ 2/7-8 object.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function fn(){ // переделать эту функцию
+    const obj1 = { name: "Rapa"};
+    let obj2 = {name: "Gimp"};
+    obj1.name = "Sem";
+    obj2.name = "Rola";
+
+    obj1.lllll = () => {return {string: 'lalal1'}};
+    obj2.internalObj2 = () => {return {string: 'lalal2'}};
+    return obj1, obj2;
+};
+console.log(fn());
+
+function createUser(name, city){
+    console.dir({name:name, city: city});
+};
+createUser('Marcus Aurelius', 'Roma');
