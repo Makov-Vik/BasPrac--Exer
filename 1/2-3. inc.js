d0ab725820feec08f61553f96a935665e7b0720a
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-11-15 12:04:27 +0200

    Update 2-3. inc.js

--- 1/2-3. inc.js
+++ 1/2-3. inc.js
@@ -1,7 +1,14 @@
-"use stric";
-
-function planet(name){
-    console.log("The second planet of the solar system is : " + name);
+function inc1(number){
+    return number +1;
 }
+const a = 5; 
+const b = inc1(a); 
+console.dir({a , b});
 
-planet("Venus");
+var Num = {n: 61};
+function inc2(Num){
+    return Num
+}
+const obj = {n: 5};
+inc1(obj);
+console.dir(obj);
