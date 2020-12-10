04cb03a3f523304866fa8d3652ddc4fbec735227
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-12-10 11:14:32 +0200

    Update Cecles.js

--- 4/Cecles.js
+++ 4/Cecles.js
@@ -8,5 +8,15 @@ function sumfor(...args){
   return console.log(count);
 }
 
+function sumforof(...args){
+  let count = 0;
+  let i = 0;
+  for (i of args){
+    count = count + i;
+  }
+  return console.log(count);
+}
+
 
 const a = sumfor(1, 2, 3) 
+const b = sumforof(0)
