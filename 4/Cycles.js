32ae3c8342242594e22e980710043753dd55a834
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-12-11 00:05:11 +0200

    Update Cycles.js

--- 4/Cycles.js
+++ 4/Cycles.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function sumfor(...args){
+function sumFor(...args){
   let count = 0;
   for (let i = 0; i < args.length; i++){
     count = count + args[i];
@@ -8,7 +8,7 @@ function sumfor(...args){
   return console.log(count);
 }
 
-function sumforof(...args){
+function sumForOf(...args){
   let count = 0;
   let i = 0;
   for (i of args){
@@ -17,6 +17,34 @@ function sumforof(...args){
   return console.log(count);
 }
 
+function sumWhile(...args){
+  let i = 0;
+  let count = 0;
+  while(i < args.length){
+    count = count + args[i];
+    i++;
+  }
+  return console.log(count);
+}
 
-const a = sumfor(1, 2, 3) 
-const b = sumforof(0)
+function sumDoWhile(...args){
+  let i = 0;
+  let count = 0;
+  do {
+    if (args.length>0){
+      count = count + args[i];
+      i++;}
+  } while (i < args.length);
+  return console.log(count);
+}
+
+function sumPrototype(...args){
+  let count = 0;
+  args.forEach(x => count += x)
+  return console.log(count);
+}
+const a = sumFor(1, 2, 3) 
+const b = sumForOf(0)
+const c = sumWhile(-1, 5) 
+const d = sumDoWhile() 
+const e = sumPrototype(10, -1, -1, -1) 
