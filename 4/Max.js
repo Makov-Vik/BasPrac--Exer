c70aa74a77dcc959346e130fce9ae111c83d4836
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-12-12 23:40:28 +0200

    Update Max.js

--- 4/Max.js
+++ 4/Max.js
@@ -1,1 +1,13 @@
+'use strict'
 
+const compute = (a,b) => (a > b ? a: b);
+
+function max(args){
+  let res = [];
+ 
+  return (args.map( row => row.reduce(compute)))
+  .reduce(compute) ;
+}
+
+const m = max([[1, 2, 3], [4, 5, 100], [7, 8, 101]]);
+console.log(m);
