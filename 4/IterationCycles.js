344f2f69b38d1fcd89825eade129f4d61e6925df
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-12-14 00:33:51 +0200

    Create IterationCycles.js

---
+++ 4/IterationCycles.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const persons = {
+  lenin: { born: 1870, died: 1924 },
+  mao: { born: 1893, died: 1976 },
+  gandhi: { born: 1869, died: 1948 },
+  hirohito: { born: 1901, died: 1989 },
+};
+
+let res = {};
+function ages(){
+  for (let i in persons){
+    let k = 0;
+    let a = 1;
+    for ( let j in persons[i]){
+      a = -1*a;
+      k = k + persons[i][j]*a; 
+    }
+    res[i] = k;
+  }
+  console.log(res);
+};
+
+ages(persons);
