23c50671977a99322cd1118bdb47029d52dd8374
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-03 01:07:35 +0200

    Update 7-8 object.js

--- 2/7-8 object.js
+++ 2/7-8 object.js
@@ -1,5 +1,5 @@
 "use strict";
-//8 exercise
+//7 exercise
 const obj1 = { name: 'Vik',};
 
 let obj2 = { name : 'Vik'};
@@ -12,7 +12,7 @@ console.log ('object 1: ' ,obj1, '. object 2: ' , obj2);
 obj2['style'] = 'panamera';
 console.log('object 2 after : ' , obj2);
 
-//9 exercise
+//8 exercise
 
 function createUser(name, city){
     let object = {};
