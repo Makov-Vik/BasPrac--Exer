f06b5965d580dc92a4b4851c642d9550e76bd1d0
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-05 20:31:00 +0200

    Update 3_ip.js

--- 3/3_ip.js
+++ 3/3_ip.js
@@ -16,3 +16,21 @@ console.log(IP('192.168.1.10'));
 console.log(IP('165.225.133.150'));
 console.log(IP('0.0.0.0'));
 console.log(IP('8.8.8.8'));
+
+// "use strict";
+
+// function IP(ip){
+//     let a = ip.split('.');
+//     for(let i in a){
+//         a[i] = (parseInt(a[i], 10));
+//     };
+    
+//     for( let i = 0; i < 3; i++){
+//         a[i] = a[i] << 8*(3-i);
+//         //console.log(a[i]);
+//     };
+    
+//     let sum =  a.reduce(function(x, y){return  x + y });
+//     console.log(sum);
+// }
+// IP('8.8.8.8');
