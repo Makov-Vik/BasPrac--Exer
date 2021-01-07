878acadb1513bed50dff93d3839c04e04f5f8727
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-07 23:04:30 +0200

    Update IteratingWithLoops.js

--- 4/IteratingWithLoops.js
+++ 4/IteratingWithLoops.js
@@ -1,50 +1,49 @@
 'use strict'
 
-function sumFor(...args){
-  let count = 0;
+function sumfor(...args){
+  let a = 0;
   for (let i = 0; i < args.length; i++){
-    count = count + args[i];
+    a += args[i];
   }
-  return console.log(count);
+  return console.log(a);
 }
 
-function sumForOf(...args){
-  let count = 0;
-  let i = 0;
-  for (i of args){
-    count = count + i;
+function sumforof(...args){
+  let a = 0;
+  for( let i of args){
+    a += i;
   }
-  return console.log(count);
+  return console.log(a);
 }
 
 function sumWhile(...args){
-  let i = 0;
-  let count = 0;
+  let i = 0, a = 0;
   while(i < args.length){
-    count = count + args[i];
+    a += args[i];
     i++;
   }
-  return console.log(count);
+  return console.log(a);
 }
-
 function sumDoWhile(...args){
-  let i = 0;
-  let count = 0;
-  do {
-    if (args.length>0){
-      count = count + args[i];
-      i++;}
-  } while (i < args.length);
-  return console.log(count);
+  let a = 0, i = 0;
+  if( args){
+    do{
+      a += args[i];
+      i++
+    }
+  while(i < args.length)
+  }
+  return console.log(a);
 }
 
-function sumPrototype(...args){
-  let count = 0;
-  args.forEach(x => count += x)
-  return console.log(count);
+function sumReduce(...args){
+    let a = 0;
+    a = args.reduce((x,y) => x +y);
+    return console.log(a);
 }
-const a = sumFor(1, 2, 3) 
-const b = sumForOf(0)
-const c = sumWhile(-1, 5) 
-const d = sumDoWhile() 
-const e = sumPrototype(10, -1, -1, -1) 
+
+const a = sumfor(1, 2, 3) // a === 6
+const b = sumforof(0) // b === 0
+const c = sumWhile() // c === 0
+const d = sumDoWhile(1, -1, 1, 5) // d === 1
+const e = sumReduce(10, -1, -1, -1) // e === 7
