32021a14ef226e1e04ec93a42fa616d88628ae65
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-08 00:44:07 +0200

    Update IterationCycles.js

--- 4/IterationCycles.js
+++ 4/IterationCycles.js
@@ -1,24 +1,43 @@
 'use strict'
 
+function ages(persons){
+  let age = {};
+  for( let i in persons){
+    age[i] = persons[i].died - persons[i].born;
+  }
+  return age 
+}
+
 const persons = {
   lenin: { born: 1870, died: 1924 },
   mao: { born: 1893, died: 1976 },
   gandhi: { born: 1869, died: 1948 },
   hirohito: { born: 1901, died: 1989 },
 };
+console.log(ages(persons));
 
-let res = {};
-function ages(){
-  for (let i in persons){
-    let k = 0;
-    let a = 1;
-    for ( let j in persons[i]){
-      a = -1*a;
-      k = k + persons[i][j]*a; 
-    }
-    res[i] = k;
-  }
-  console.log(res);
-};
+// 'use strict'
 
-ages(persons);
+// const persons = {
+//   lenin: { born: 1870, died: 1924 },
+//   mao: { born: 1893, died: 1976 },
+//   gandhi: { born: 1869, died: 1948 },
+//   hirohito: { born: 1901, died: 1989 },
+// };
+
+// let res = {};
+// function ages(){
+//   for (let i in persons){
+//     let k = 0;
+//     let a = 1;
+//     for ( let j in persons[i]){
+//       a = -1*a;
+//       k = k + persons[i][j]*a; 
+//     }
+//     res[i] = k;
+//   }
+//   console.log(res);
+// };
+
+// ages(persons);
+
