cf87dd98cb2c74063ebc9f0d130edca51bd94c3e
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-07 23:42:43 +0200

    Update IteratingTwoArray.js

--- 4/IteratingTwoArray.js
+++ 4/IteratingTwoArray.js
@@ -11,3 +11,19 @@ function max(args){
 
 const m = max([[1, 2, 3], [4, 5, 100], [7, 8, 101]]);
 console.log(m);
+
+// 'use strict'
+
+// function max(arg){
+//   let max = null;
+//   for( let i of arg){
+//     for (let j of i){
+//       j > max ? max = j : j = j
+//     }
+//   }
+//   return max
+// }
+
+// const m = max([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+// console.log(m); // 9
+
