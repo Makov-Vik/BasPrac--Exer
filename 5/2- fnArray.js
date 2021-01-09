505fa803a13013bc2153cd36acb52665fb3549bd
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-09 23:31:15 +0200

    Create 2- fnArray.js

---
+++ 5/2- fnArray.js
@@ -0,0 +1,25 @@
+'use sctrict'
+
+function array(){
+  let any = x => arr[x];
+  let arr = [];
+  any.push = x => arr.push(x)
+  any.pop = x => arr.pop(x)
+  return any
+}
+
+const arr = array();
+
+arr.push('first');
+arr.push('second');
+arr.push('third');
+
+console.log(arr(0)); // Выводит: first
+console.log(arr(1)); // Выводит: second
+console.log(arr(2)); // Выводит: third
+
+console.log(arr.pop()); // Выводит: third
+console.log(arr.pop()); // Выводит: second
+console.log(arr.pop()); // Выводит: first
+
+console.log(arr.pop()); // Выводит: undefined
