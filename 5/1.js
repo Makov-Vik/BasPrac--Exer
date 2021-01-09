dea5658423294f2f86cfecc41317f20de642b629
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-09 16:28:27 +0200

    Update 1.js

--- 5/1.js
+++ 5/1.js
@@ -1,6 +1,6 @@
 'use strict'
 
 const seq = a => b =>
- typeof b !== 'number' ? seq(x => a(b(x))) : console.log((a(b))) 
+ typeof b !== 'number' ? seq(x => a(b(x))) : console.log(a(b)) 
 
 seq(x => x + 7)(x => x * 2)(5)
