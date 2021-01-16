7a57c89b504a66a44ff49c8a761b3fcf28387513
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2021-01-16 16:21:42 +0200

    Update 3-without repeats.js

--- 7-Array/3-without repeats.js
+++ 7-Array/3-without repeats.js
@@ -7,3 +7,6 @@ const unique = (array) => {
 
 const result = unique([2, 1, 1, 3, 2]);
 console.log(result);
+
+const resultstring = unique(['top', 'bottom', 'top', 'left']);
+console.log(resultstring);
